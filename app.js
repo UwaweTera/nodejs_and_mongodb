a3aea2d80c2e01edc5f52e1078254efcade781fe
uwawetera <uwaweteraviv@gmail.com>
2023-01-11 11:35:13 +0530

    changes to app js

--- app.js
+++ app.js
@@ -1,34 +1,60 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import "dotenv/config";
-import router from './src/routes/articleRoutes.js';
-import session from 'express-session';
-import router2 from './src/routes/contactRoutes.js';
-import router3 from './src/routes/regRoutes.js';
+import mongoose from 'mongoose';
+import passport from 'passport';
+import swaggerJSDoc from 'swagger-jsdoc';
+import SwaggerUI from 'swagger-ui-express';
+import router from './src/routes/routes';
 
 
+const dbURI = process.env.DBURI;
 const app = express();
-app.use("/",router);
-        app.use('/',router2);
-        app.use('/',router3);
-        app.use(express.json());
-        app.use(session({
-            secret: 'keyboard cat',
-            resave: false,
-            saveUninitialized: true
-        }))
-app.use(express.urlencoded({extended: false}));
-const port = process.env.PORT;
-try{
-    mongoose.set('strictQuery', true);
-    mongoose.connect(process.env.REMOTEDB,{useNewUrlParser: true}).then(()=>{
-        app.listen(port,()=>{
-            console.log(`The server is running on: ${port}`);
-        })
-    }).catch((err)=>{
-        console.log("This Error: ",err.message);
-    })
-    
-}catch(error){
-    console.log(error);
+
+const options = {
+    swaggerDefinition:{    
+        openapi: '3.0.0',
+        info: {
+            title: 'My brand api',
+            version: '1.0.0',
+            description: 'Simple express api'
+        },
+        servers: [{
+            url : "http://localhost:2000"
+        }],
+        components: {
+           securitySchemes:{
+            bearerAuth: {
+                type: "http",
+                scheme: "bearer",
+                bearerFormat: "JWT"
+            },
+            security:[
+                {
+                    bearerAuth: [] 
+                }   
+            ]
+                  
+           }
+            
+        }  
+    },
+    apis: ['./src/routes/api/*.js']
 }
+
+const specs = swaggerJSDoc(options);
+
+app.use('/api-docs',SwaggerUI.serve,SwaggerUI.setup(specs))
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(passport.initialize());
+app.use("/",router);
+
+    mongoose.set('strictQuery', true);
+    mongoose.connect(process.env.REMOTEDB,{useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+        console.log('connected to database')
+    }).catch((err)=>{
+        console.log(err)
+    })
+
+
+export default app
