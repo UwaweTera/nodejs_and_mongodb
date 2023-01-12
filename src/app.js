0001c5e2e5043d52c8c27439acdf7bdebd5058fb
uwawetera <uwaweteraviv@gmail.com>
2023-01-12 09:16:33 +0530

    Other changes to port

--- src/app.js
+++ src/app.js
@@ -47,12 +47,10 @@ app.use(express.urlencoded({extended: false}));
 app.use(passport.initialize());
 app.use("/",router);
 
-const port = process.env.PORT || 7050;
-const dbURI = process.env.DBURI;
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.REMOTEDB,{useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
-    app.listen(port,()=>{
-        console.log(`The server is running on: ${port}`);
+    app.listen(process.env.PORT,()=>{
+        console.log(`The server is running on: ${process.env.PORT}`);
     })
 }).catch((err)=>{
     console.log(err)
